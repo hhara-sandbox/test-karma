a53e99c30098549a77e3bf48b4e456e81b2bbab2
takuhito-h <bluesbug@gmail.com>
2014-08-08 14:08:25 +0900

    gruntでkarmaが走らせられるように修正

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,38 +5,11 @@ module.exports = function (grunt) {
 
     grunt.initConfig({
         pkg: pkg,
-        // power-assert用にコードを変換するタスク
-        espower: {
-            test: {
-                files: [
-                    {
-                        expand: true, 
-                        cwd: 'test/',             // 変換対象のファイルを配置しているディレクトリ
-                        src: ['*.js'],            // 変換対象のファイル名
-                        dest: 'test/espowered/',  // 変換後のコードを配置するディレクトリ
-                        ext: '.js'                // 変換後のファイルの拡張子
-                    }
-                ]
-            }
-        },
-        // ファイルの変更検知用タスク
-        esteWatch: {
-            options: {
-                dirs: ['js', 'test'],
-                livereload: false
-            },
-            js: function (filepath) {
-                return ['test'];
-            }
-        },
         // Karma実行用タスク
         karma: {
             unit: {
                 options: {
                     configFile: 'karma.conf.js',
-                    autoWatch: true,
-                    browsers: ["Chrome"],
-                    reporters: ["progress"],
                     singleRun: true,
                     keepalive: true
                 }
