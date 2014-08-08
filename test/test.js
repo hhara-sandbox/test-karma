eacc2e60fc8c8f792f1d1833020f8b93bea09a7d
takuhito-h <bluesbug@gmail.com>
2014-08-08 21:28:02 +0900

    reporterをmochaに変更

--- test/test.js
+++ test/test.js
@@ -6,7 +6,7 @@ console.log(target.returnPlus, assert);
 describe("target", function () {
     describe("returnPlus", function () {
         it("２つの引数を足した値を返すこと", function () {
-            assert(target.returnPlus(1,2) === 3);
+            assert(target.returnPlus(1, 2) === 3);
         });
     });
 });
